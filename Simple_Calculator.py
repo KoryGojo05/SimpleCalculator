cd31f83dc26cfdf8dcd5ee2dec6a5ad995895077
AlexFroste05 <95540308+Kory05@users.noreply.github.com>
2023-07-05 06:23:15 +0200

    Main program

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -16,8 +16,8 @@ def lang(lng):
         maxlng=int(4)
         msglng1=str("\n1. English")
         msglng2=str("\n2. Spanish")
-        #msglng3=str("\n3. French")
-        #msglng4=str("\n4. Deutsch")
+        msglng3=str("\n3. French")
+        msglng4=str("\n4. Deutsch")
         print(msglng1+msglng2)
         numlng=input("Write the number of language you want")
         try:
@@ -52,6 +52,7 @@ def EN_Calc():
         while opr!=0:
                 print(oprmsg0+oprmsg1+oprmsg2+oprmsg3+oprmsg4+oprmsg5+oprmsg6)
                 opr=input("\nWrite the number of operation you want to do: ")
+
                 try:
                         opr=int(opr)
                         if opr<0 or opr>maxopr:
@@ -65,35 +66,41 @@ def EN_Calc():
                         num2=float(input("Write another number: "))
                         sumt=num1+num2
                         print("The sum of",num1,"and",num2,"is",sumt)
+
                 elif opr==2:
                         print(oprmsg2)
                         num1=float(input("Write a number: "))
                         num2=float(input("Write another number: "))
                         subt=num1-num2
                         print("Subtract of",num1,"and",num2,"is",subt)
+
                 elif opr==3:
                         print(oprmsg3)
                         num1=float(input("Write a number: "))
                         num2=float(input("Write another number: "))
                         multi=num1*num2
                         print("Multiplication of",num1,"by",num2,"is",multi)
+
                 elif opr==4:
                         print(oprmsg4)
                         num1=float(input("Write a number: "))
                         num2=float(input("Write another number: "))
                         div=num1/num2
                         print("The division of",num1,"between",num2,"is",div)
+
                 elif opr==5:
                         print(oprmsg5)
                         num1=float(input("Write the power base: "))
                         num2=float(input("Write the exponent of power: "))
                         pot=num1**num2
                         print("The power of",num1,"raised to",num2,"is",pot)
+
                 elif opr==6:
                         print(oprmsg6)
                         num1=float(input("Write a number: "))
                         square=sqrt(num1)
                         print("The square root of",num1,"is",square)
+
                 wait=0
                 while wait!=10000000:
                         wait+=1
@@ -101,10 +108,11 @@ def EN_Calc():
                         next=input("Press \"Enter\" to continue...")
                         while next!="":
                                 next=input("ERROR: Press \"Enter\" to continue...")
-        print(" \nFinalizando Programa...")
+
+        print(" \nFinishing Program...")
         while wait!=10000000:
                 wait=wait+1
-        print("Programa Finalizado")
+        print("The program finished")
         wait=0
         while wait!=5000000:
                 wait=wait+1
@@ -125,6 +133,7 @@ def ES_Calc():
         while opr!=0:
                 print(oprmsg0+oprmsg1+oprmsg2+oprmsg3+oprmsg4+oprmsg5+oprmsg6)
                 opr=input("\nEscribe el número del cálculo que quieres hacer: ")
+
                 try:
                         opr=int(opr)
                         if opr<0 or opr>maxopr:
@@ -132,41 +141,48 @@ def ES_Calc():
                 except:
                         opr=-1
                         print("\nERROR: El carácter indicado no se encuentra en la lista.")
+
                 if opr==1:
                         print(oprmsg1)
                         num1=float(input("Escribe un número: "))
                         num2=float(input("Escribe otro número: "))
                         sumt=num1+num2
                         print("La suma de",num1,"y de",num2,"es",sumt)
+
                 elif opr==2:
                         print(oprmsg2)
                         num1=float(input("Escribe un número: "))
                         num2=float(input("Escribe otro número: "))
                         resta=num1-num2
                         print("La resta de",num1,"y de",num2,"es",resta)
+
                 elif opr==3:
                         print(oprmsg3)
                         num1=float(input("Escribe un número: "))
                         num2=float(input("Escribe otro número: "))
                         multi=num1*num2
                         print("La multiplicación de",num1,"por",num2,"es",multi)
+
                 elif opr==4:
                         print(oprmsg4)
                         num1=float(input("Escribe un número: "))
                         num2=float(input("Escribe otro número: "))
                         div=num1/num2
                         print("La división de",num1,"entre",num2,"es",div)
+
                 elif opr==5:
                         print(oprmsg5)
                         num1=float(input("Escribe la base de la potencia: "))
                         num2=float(input("Escribe el exponente de la potencia: "))
                         pot=num1**num2
                         print("La potencia de",num1,"elevado a",num2,"es:",pot)
+
                 elif opr==6:
                         print(oprmsg6)
                         num1=float(input("Escribe un número: "))
                         raiz=sqrt(num1)
                         print("La raiz cuadrada de",num1,"es:",raiz)
+
                 wait=0
                 while wait!=10000000:
                         wait+=1
@@ -186,7 +202,7 @@ def ES_Calc():
 
 
 def FR_Calc():
-        
+
         return()
 
 
