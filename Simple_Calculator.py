35a6a230649b0aeb5f629329997e89e4db677c17
Alex.Froste_05 <95540308+AlexFroste05@users.noreply.github.com>
2023-09-13 19:10:49 +0200

    Update language sistem

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -8,10 +8,11 @@ lng="EN"
 
 LANGUAGES = { "EN":{"msgpro0":"\n0.Finish Program", "msgpro1":"\n1. Select Language", "msgpro2":"\n2. Calculator", "opr":"\nWrite the number of operation you want to do: ", "oprmsg0":"0. Finish Program", 
                     "oprmsg1":"1. Addition", "oprmsg2":"2. Subtraction", "oprmsg3": "3. Multiplication", "oprmsg4":"4. Division", "oprmsg5":"5. Power", "oprmsg6":"6. Square Root", 
-                    "ERRORnum":"ERROR: That isn't a number.", "num1":"Write a number: ", "num2":"Write another number: ", "next":"Press \"Enter\" to continue...", "ERRORnext":"ERROR: Press \"Enter\" to continue...", "finish1":"\nFinishing Program...","finish2":"The program finished", 
+                    "ERRORnum":"ERROR: That isn't a number.", "num1":"Write a number: ", "num2":"Write another number: ", "next":"Press \"Enter\" to continue...", "ERRORnext":"ERROR: Press \"Enter\" to continue...", "finish1":"\nFinishing Program...","finish2":"The program finished", "ERRORopr":"\nERROR: The indicated number isn't listed.", "pownum1":"Write the power base: ",
+                    "pownum2":"Write the exponent of power: ", "numlng":"Write the number of language you want: ", 
                     },
                 "ES": {"oprmsg0":"0. Finalizar Programa", "oprmsg1":"1. Sumar", "oprmsg2":"2. Resta", "oprmsg3": "3. Multiplicación", "oprmsg4":"4. División", "oprmsg5":"5. Potencia", "oprmsg6":"6. Raíz Cuadrada", 
-                    "ERRORnum":"ERROR: That isn't a number.","num1":"Escribe un número: ","num2":"Escribe otro número: ", "next":"Presiona \"Enter\" para continuar...", "ERRORnext":"ERROR: Presiona \"Enter\" para continuar...",
+                    "ERRORnum":"ERROR: El valor introducido no es un número.","num1":"Escribe un número: ","num2":"Escribe otro número: ", "next":"Presiona \"Enter\" para continuar...", "ERRORnext":"ERROR: Presiona \"Enter\" para continuar...", "numlng":"Escribe el número del idioma: ",
                     },
                 "FR": {
 
@@ -29,15 +30,15 @@ def EN_Calc():
         while opr!=0:
                 for opt_menu in range(0,7):
                         print(LANGUAGES[lng][f'oprmsg{opt_menu}'])
-                opr=input("\nWrite the number of operation you want to do: ")
+                opr=input(LANGUAGES[lng]["opr"])
 
                 try:
                         opr=int(opr)
                         if opr<0 or opr>maxopr:
-                                print("\nERROR: The indicated number isn't listed.")
+                                print(LANGUAGES[lng]["ERRORopr"])
                 except:
                         opr=-1
-                        print("\nERROR: The indicated number isn't listed.")
+                        print(LANGUAGES[lng]["ERRORopr"])
                 if opr==1:
                         print(LANGUAGES[lng][f"oprmsg{opr}"])
                         correct=False
@@ -82,8 +83,8 @@ def EN_Calc():
 
                 elif opr==5:
                         print(LANGUAGES[lng][f"oprmsg{opr}"])
-                        num1=float(input("Write the power base: "))
-                        num2=float(input("Write the exponent of power: "))
+                        num1=float(input(LANGUAGES[lng]["pownum1"]))
+                        num2=float(input(LANGUAGES[lng]["pownum2"]))
                         pot=num1**num2
                         print("The power of",num1,"raised to",num2,"is",pot)
 
@@ -204,7 +205,7 @@ while pro!=0:
                 msglng3=str("\n3. French")
                 msglng4=str("\n4. Deutsch")
                 print(msglng1+msglng2)
-                numlng=input("Write the number of language you want: ")
+                numlng=input(LANGUAGES[lng]["numlng"])
                 try:
                         numlng=int(numlng)
                         if numlng<0 or numlng>maxlng:
@@ -226,12 +227,7 @@ while pro!=0:
                         EN_Calc()
                 elif lng=="ES":
                         ES_Calc()
-                elif lng=="FR":
-                        fr=0
-                        #FR_Calc()
-                elif lng=="DE":
-                        de=0
-                        #DE_Calc()
+
 
 print("\nFinishing Program...")
 wait=0
