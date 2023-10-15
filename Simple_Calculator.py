9dd52205264dd5e98355ef41f79f4c09f9c1738d
Alex.Froste_05 <95540308+AlexFroste05@users.noreply.github.com>
2023-10-15 18:51:42 +0200

    Update code

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -1,18 +1,26 @@
 #Program that performs the 6 main mathematical calculations
-#Addition, Subtraction, Multiplication, Division, Power, Square Root
+#Addition, Subtraction, Multiplication, Division, Power, Square Root
+
 
 pro=int(-1)
 lng="EN"
 
-LANGUAGES = { "EN":{"msgpro0":"\n0.Finish Program", "msgpro1":"\n1. Select Language", "msgpro2":"\n2. Calculator", "opr":"\nWrite the number of operation you want to do: ", "oprmsg0":"0. Finish Program", 
-                    "oprmsg1":"1. Addition", "oprmsg2":"2. Subtraction", "oprmsg3": "3. Multiplication", "oprmsg4":"4. Division", "oprmsg5":"5. Power", "oprmsg6":"6. Square Root", "ERRORnum":"ERROR: That isn't a number.", 
-                    "num1":"Write a number: ", "num2":"Write another number: ", "next":"Press \"Enter\" to continue...", "ERRORnext":"ERROR: Press \"Enter\" to continue...", "finish1":"\nFinishing Program...","finish2":"The program finished", 
-                    "ERRORopr":"\nERROR: The indicated number isn't listed.", "pownum1":"Write the power base: ","pownum2":"Write the exponent of power: ", "numlng":"Write the number of language you want: ", 
+LANGUAGES = { "EN":{"msgpro0":"\n0.Finish Program", "msgpro1":"\n1. Select Language", "msgpro2":"\n2. Calculator",
+                    "opr":"\nWrite the number of operation you want to do: ", "oprmsg0":"0. Finish Program", 
+                    "oprmsg1":"1. Addition", "oprmsg2":"2. Subtraction", "oprmsg3": "3. Multiplication", 
+                    "oprmsg4":"4. Division", "oprmsg5":"5. Power", "oprmsg6":"6. Square Root", 
+                    "ERRORnum":"ERROR: That isn't a number.", "num1":"Write a number: ", "num2":"Write another number: ",
+                    "next":"Press \"Enter\" to continue...", "ERRORnext":"ERROR: Press \"Enter\" to continue...",
+                    "finish1":"\nFinishing Program...","finish2":"The program finished",
+                    "ERRORopr":"\nERROR: The indicated number isn't listed.", "pownum1":"Write the power base: ",
+                    "pownum2":"Write the exponent of power: ", "numlng":"Write the number of language you want: ", 
                     },
-                "ES": {"msgpro0":"\n0.Finish Program", "msgpro1":"\n1. Select Language", "msgpro2":"\n2. Calculator","oprmsg0":"0. Finalizar Programa", 
-                    "oprmsg1":"1. Sumar", "oprmsg2":"2. Resta", "oprmsg3": "3. Multiplicación", "oprmsg4":"4. División", "oprmsg5":"5. Potencia", "oprmsg6":"6. Raíz Cuadrada", 
-                    "ERRORnum":"ERROR: El valor introducido no es un número.","num1":"Escribe un número: ","num2":"Escribe otro número: ", "next":"Presiona \"Enter\" para continuar...", 
-                    "ERRORnext":"ERROR: Presiona \"Enter\" para continuar...", "numlng":"Escribe el número del idioma: ",
+                "ES": {"msgpro0":"\n0.Finish Program", "msgpro1":"\n1. Select Language", "msgpro2":"\n2. Calculator",
+                        "oprmsg0":"0. Finalizar Programa", "oprmsg1":"1. Sumar", "oprmsg2":"2. Resta",
+                        "oprmsg3": "3. Multiplicación", "oprmsg4":"4. División", "oprmsg5":"5. Potencia",
+                        "oprmsg6":"6. Raíz Cuadrada","ERRORnum":"ERROR: El valor introducido no es un número.",
+                        "num1":"Escribe un número: ","num2":"Escribe otro número: ", "next":"Presiona \"Enter\" para continuar...",
+                        "ERRORnext":"ERROR: Presiona \"Enter\" para continuar...", "numlng":"Escribe el número del idioma: ",
                     },
                 "FR": {
 
