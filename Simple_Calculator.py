cc6c9effcc19ceb32d323248b47cfbf41ee5169c
AlexFroste05 <95540308+Kory05@users.noreply.github.com>
2024-04-17 18:17:26 +0200

    Update Language sistem

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -4,13 +4,6 @@
 pro = int(-1)
 lng = "EN"
 
-def langlist():
-    msglng1 = str("\n1. English")
-    msglng2 = str("\n2. Spanish")
-    msglng3 = str("\n3. French")
-    msglng4 = str("\n4. Deutsch")
-    return()
-
 LANGUAGES = {"EN": {"msgpro0": "\n0.Finish Program", "msgpro1": "\n1. Select Language", "msgpro2": "\n2. Calculator",
                     "opr": "\nWrite the number of operation you want to do: ", "oprmsg0": "0. Finish Program",
                     "oprmsg1": "1. Addition", "oprmsg2": "2. Subtraction", "oprmsg3": "3. Multiplication",
@@ -22,21 +15,23 @@ LANGUAGES = {"EN": {"msgpro0": "\n0.Finish Program", "msgpro1": "\n1. Select Lan
                     "ERRORopr": "\nERROR: The indicated number isn't listed.", "pownum1": "Write the power base: ",
                     "pownum2": "Write the exponent of power: ", "numlng": "Write the number of language you want: ",
                     "msgsum": "The sum de, %s y de %s es %s", "msgsub": "The subtract of %s and %s is %s",
-                    "msgmul":"Multiplication of %s by %s is %s", "msgdiv":"The division of %s between %s is %s",
-                    "msgpow":"The power of %s raised to %s is %s", "msgsqr":"The square root of %s is %s",
+                    "msgmul": "Multiplication of %s by %s is %s", "msgdiv": "The division of %s between %s is %s",
+                    "msgpow": "The power of %s raised to %s is %s", "msgsqr": "The square root of %s is %s",
                     },
              "ES": {"msgpro0": "\n0. Finalizar Programa", "msgpro1": "\n1. Seleccionar Idioma",
-                    "msgpro2": "\n2. Calculadora", "opr":"Escribe el número del cálculo que quieres hacer: ",
-                    "ERRORopr":"ERROR: El carácter indicado no es un número.", "oprmsg0": "0. Finalizar Programa",
+                    "msgpro2": "\n2. Calculadora", "numlng": "Escribe el número del idioma: ",
+                    "opr": "Escribe el número del cálculo que quieres hacer: ",
+                    "ERRORopr": "ERROR: El carácter indicado no es un número.", "oprmsg0": "0. Finalizar Programa",
                     "oprmsg1": "1. Sumar", "oprmsg2": "2. Resta", "oprmsg3": "3. Multiplicación",
                     "oprmsg4": "4. División", "oprmsg5": "5. Potencia", "oprmsg6": "6. Raíz Cuadrada",
                     "ERRORnum": "ERROR: El valor introducido no es un número.", "num1": "Escribe un número: ",
                     "num2": "Escribe otro número: ", "next": "Presiona \"Enter\" para continuar...",
                     "ERRORnext": "ERROR: Presiona \"Enter\" para continuar...",
-                    "numlng": "Escribe el número del idioma: ",
-                    "msgsum": "La suma de, %s y de %s es %s", "msgsub":"La resta de, %s menos %s es %s",
-                    "msgmul":"La multiplicación de %s por %s es %s","msgdiv":"La división de %s entre %s es %s",
-                    "msgpow":"La potencia de %s elevado a %s es %s","msgsqr":"",
+                    "finish1": "\nFinalizando Programa...", "finish2": "Programa finalizado",
+                    "pownum1": "Escribe la base de la potencia: ", "pownum2": "Escribe el exponente de la potencia: ",
+                    "msgsum": "La suma de %s y de %s es %s", "msgsub": "La resta de %s menos %s es %s",
+                    "msgmul": "La multiplicación de %s por %s es %s", "msgdiv": "La división de %s entre %s es %s",
+                    "msgpow": "La potencia de %s elevado a %s es %s", "msgsqr": "La raiz cuadrada de %s es: %s",
                     },
              "FR": {
 
@@ -290,7 +285,11 @@ while pro != 0:
         print("\nERROR: The indicated number isn't listed.")
     if pro == 1:
         maxlng = int(2)
-        print(langlist())
+        msglng1 = str("\n1. English")
+        msglng2 = str("\n2. Spanish")
+        msglng3 = str("\n3. French")
+        msglng4 = str("\n4. Deutsch")
+        print(msglng1 + msglng2)
         numlng = input(LANGUAGES[lng]["numlng"])
         try:
             numlng = int(numlng)
