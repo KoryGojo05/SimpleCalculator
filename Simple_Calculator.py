87942b2ee165fa46dd12357611bd9777236f6398
KoryGojo05 <95540308+Kory05@users.noreply.github.com>
2024-08-21 20:09:00 +0200

    Update

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -4,13 +4,13 @@
 pro = int(-1)
 lng = "EN"
 
-LANGUAGES = {"EN": {"msgpro0": "\n0.Finish Program", "msgpro1": "\n1. Select Language",
+LANGUAGES = {"EN": {"msgpro0": "\n0. Finish Program", "msgpro1": "\n1. Select Language",
                     "msgpro2": "\n2. Calculator",
-                    "opr": "\nWrite the number of operation you want to do: ",
-                    "oprmsg0": "\t0. Finish Program", "oprmsg1": "\t1. Addition",
-                    "oprmsg2": "\t2. Subtraction", "oprmsg3": "\t3. Multiplication",
-                    "oprmsg4": "\t4. Division", "oprmsg5": "\t5. Power",
-                    "oprmsg6": "\t6. Square Root",
+                    "opr": "Write the number of operation you want to do: ",
+                    "oprmsg0": "0. Finish Program", "oprmsg1": "1. Addition",
+                    "oprmsg2": "2. Subtraction", "oprmsg3": "3. Multiplication",
+                    "oprmsg4": "4. Division", "oprmsg5": "5. Power",
+                    "oprmsg6": "6. Square Root",
                     "ERRORnum": "ERROR: That isn't a number.",
                     "num1": "Write a number: ", "num2": "Write another number: ",
                     "next": "Press \"Enter\" to continue...",
@@ -26,9 +26,10 @@ LANGUAGES = {"EN": {"msgpro0": "\n0.Finish Program", "msgpro1": "\n1. Select Lan
                     "msgdiv": "The division of %s between %s is %s",
                     "msgpow": "The power of %s raised to %s is %s",
                     "msgsqr": "The square root of %s is %s",
+                    "msglng1": "\t1. English", "msglng2": "\t2. Spanish",
                     },
              "ES": {"msgpro0": "\n0. Finalizar Programa", "msgpro1": "\n1. Seleccionar Idioma",
-                    "msgpro2": "\n2. Calculadora", "numlng": "Escribe el número del idioma: ",
+                    "msgpro2": "\n2. Calculadora",
                     "opr": "Escribe el número del cálculo que quieres hacer: ",
                     "ERRORopr": "ERROR: El carácter indicado no es un número.",
                     "oprmsg0": "0. Finalizar Programa", "oprmsg1": "1. Sumar",
@@ -36,7 +37,7 @@ LANGUAGES = {"EN": {"msgpro0": "\n0.Finish Program", "msgpro1": "\n1. Select Lan
                     "oprmsg4": "4. División", "oprmsg5": "5. Potencia",
                     "oprmsg6": "6. Raíz Cuadrada",
                     "ERRORnum": "ERROR: El valor introducido no es un número.",
-                    "num1": "Escribe un número: ",
+                    "num1": "Escribe un número: ", "numlng": "Escribe el número del idioma: ",
                     "num2": "Escribe otro número: ", "next": "Presiona \"Enter\" para continuar...",
                     "ERRORnext": "ERROR: Presiona \"Enter\" para continuar...",
                     "finish1": "\nFinalizando Programa...", "finish2": "Programa Finalizado",
@@ -48,6 +49,7 @@ LANGUAGES = {"EN": {"msgpro0": "\n0.Finish Program", "msgpro1": "\n1. Select Lan
                     "msgdiv": "La división de %s entre %s es %s",
                     "msgpow": "La potencia de %s elevado a %s es %s",
                     "msgsqr": "La raiz cuadrada de %s es: %s",
+                    "msglng1": "\t1. Inglés", "msglng2": "\t2. Español",
                     },
              "FR": {
 
@@ -65,7 +67,7 @@ def calc():
     while opr != 0:
         print("=" * 50)
         for opt_menu in range(0, 7):
-            print(LANGUAGES[lng][f'oprmsg{opt_menu}'])
+            print("\t" + LANGUAGES[lng][f'oprmsg{opt_menu}'])
         print("=" * 50)
         opr = input(LANGUAGES[lng]["opr"])
 
@@ -214,7 +216,7 @@ def calc():
 
 while pro != 0:
     print(LANGUAGES[lng]["msgpro0"] + LANGUAGES[lng]["msgpro1"] + LANGUAGES[lng]["msgpro2"])
-    pro = input("\nWrite the number of operation you want to do: ")
+    pro = input("\n" + LANGUAGES[lng]["opr"])
     try:
         pro = int(pro)
         if pro < 0 or pro > 2:
@@ -224,13 +226,16 @@ while pro != 0:
         print("\nERROR: The indicated number isn't listed.")
     if pro == 1:
         maxlng = int(2)
-        msglng1 = str("\n\t1. English")
-        msglng2 = str("\n\t2. Spanish")
-        msglng3 = str("\n3. French")
-        msglng4 = str("\n4. Deutsch")
+        """
+        msglng1 = str("1. English")
+        msglng2 = str("2. Spanish")
+        msglng3 = str("3. French")
+        msglng4 = str("4. Deutsch")
+        """
         numlng = -1
         while numlng <= 0 or numlng > maxlng:
-            print("=" * 50 + msglng1 + msglng2 + "\n" + "=" * 50)
+            print("=" * 50 + "\n" + LANGUAGES[lng]["msglng1"])
+            print(LANGUAGES[lng]["msglng2"] + "\n" + "=" * 50)
             numlng = input(LANGUAGES[lng]["numlng"])
             numlng = int(numlng)
             try:
